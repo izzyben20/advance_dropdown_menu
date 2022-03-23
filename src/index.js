2ea5c1ffa51348ea1b9fe35470eed5e6dd5e9936
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-03-23 01:25:23 +0100

    adjust render method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
